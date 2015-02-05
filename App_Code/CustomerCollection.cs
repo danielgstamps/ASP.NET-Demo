2ad406658c0d22cf75f9449067c96d66bcd15dd9
dstamps <danielgstamps@gmail.com>
2015-02-05 14:37:14 -0500

    Renamed CustomerList.cs to CustomerCollection.cs to avoid confusion with CustomerList.aspx. Added functionality to add customers to the CustomerCollection with duplicate-checking and appropriate error messages.

---
+++ App_Code/CustomerCollection.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.ServiceModel.Channels;
+using System.Web;
+
+/// <summary>
+/// Summary description for CustomerCollection
+/// </summary>
+public class CustomerCollection
+{
+    private List<Customer> _customerCollection;
+
+    public CustomerCollection()
+    {
+        this._customerCollection = new List<Customer>();
+    }
+
+    public Customer this[int index]
+    {
+        get { return this._customerCollection[index]; }
+        set
+        {
+            Trace.Assert(value != null, "Must enter a valid Customer object.");
+            this._customerCollection[index] = value;
+        }
+    }
+
+    public Customer this[string name]
+    {
+        get
+        {
+            return this._customerCollection.Find(customer => customer.Name == name);
+        }
+    }
+
+    public int Count()
+    {
+        return this._customerCollection.Count;
+    }
+
+    public void AddItem(Customer newCustomer)
+    {
+        this._customerCollection.Add(newCustomer);
+    }
+
+    public void RemoveAt(int index)
+    {
+        this._customerCollection.RemoveAt(index);
+    }
+
+    public void Clear()
+    {
+        this._customerCollection.Clear();
+    }
+
+    public static CustomerCollection GetCustomers()
+    {
+        if (HttpContext.Current.Session["CustomerList"] == null)
+        {
+            HttpContext.Current.Session["CustomerList"] = new CustomerCollection();
+        }
+
+        return (CustomerCollection)HttpContext.Current.Session["CustomerList"];
+    }
+}
