e40b8fd64169d07592e9acc3563ba6f6c447d7aa
dstamps <dstamps@localhost>
2015-04-04 20:11:31 -0400

    Reformatted some comments.

--- IncidentDisplay.aspx.cs
+++ IncidentDisplay.aspx.cs
@@ -1,7 +1,21 @@
 ﻿using System;
 
+/// <summary>
+/// Code-behind file for IncidentDisplay.
+/// </summary>
+/// <author> 
+/// Daniel Stamps 
+/// </author>
+/// <version> 
+/// Spring 2015
+/// </version>
 public partial class IncidentDisplay : System.Web.UI.Page
 {
+    /// <summary>
+    /// Handles the Load event of the Page control.
+    /// </summary>
+    /// <param name="sender">The source of the event.</param>
+    /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
     protected void Page_Load(object sender, EventArgs e)
     {
 
