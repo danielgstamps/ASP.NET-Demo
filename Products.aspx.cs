d8bde52c0f4be2e6b813608e06abbf14eb2932b4
dstamps <danielgstamps@gmail.com>
2015-04-04 20:11:31 -0400

    Reformatted some comments.

--- Products.aspx.cs
+++ Products.aspx.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
+/// <summary>
+/// Code-behind file for Products.
+/// </summary>
+/// <author> 
+/// Daniel Stamps 
+/// </author>
+/// <version> 
+/// Spring 2015
+/// </version>
 public partial class Products : System.Web.UI.Page
 {
+    /// <summary>
+    /// Handles the Load event of the Page control.
+    /// </summary>
+    /// <param name="sender">The source of the event.</param>
+    /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
     protected void Page_Load(object sender, EventArgs e)
     {
 
     }
 
+    /// <summary>
+    /// Handles the Click event of the btnAddSoftware control.
+    /// </summary>
+    /// <param name="sender">The source of the event.</param>
+    /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
     protected void btnAddSoftware_Click(object sender, EventArgs e)
     {
 
