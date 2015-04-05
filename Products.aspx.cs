564832bc2aaf86b9e3178c02b99e84f6a2eeb149
dstamps <dstamps@localhost>
2015-04-04 20:06:00 -0400

    Product Page: Finished GridView to display/edit/delete Software. Created table for adding new Software.

--- Products.aspx.cs
+++ Products.aspx.cs
@@ -1,13 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 public partial class Products : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnAddSoftware_Click(object sender, EventArgs e)
     {
 
     }
